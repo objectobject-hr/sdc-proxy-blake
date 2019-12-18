45e878c41d8d3ef0c4b8ad466530506540473db3
Blake Mason <blakelmason@gmail.com>
2019-12-17 20:13:48 -0800

    npmi script

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { spawn } = require('child_process')
+const { execSync } = require('child_process')
 
 const members = ['aaron', 'blake', 'david', 'steven']
 const services = members.map(member => `sdc-service-${member}`)
@@ -7,7 +7,14 @@ const tasks = {
   npmi: function(cb) {
     services.map(service => {
       console.log(`installing packages for ${service}`)
-      const child = spawn(`cd ${service} && npm i`)
+      execSync(`cd ${service} && npm i`, { stdio: 'inherit' })
+      console.log('\n----------\n')
+    })
+    cb()
+  },
+  seed: function(cb) {
+    services.map(service => {
+      console.log(`seeding database for ${service}`)
     })
   }
 }
