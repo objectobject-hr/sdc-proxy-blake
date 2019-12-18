2a53ba2334225d4ecfd7ca6e43ffacb3599307ea
Blake Mason <blakelmason@gmail.com>
2019-12-18 14:03:50 -0800

    gulp pull

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,36 @@ const services = members.map(member => `sdc-service-${member}`)
 
 const tasks = {
   npmi: function(cb) {
-    services.map(service => {
-      console.log(`installing packages for ${service}`)
-      execSync(`cd ${service} && npm i`, { stdio: 'inherit' })
-      console.log('\n----------\n')
-    })
+    cdServices('npm i')
     cb()
   },
   seed: function(cb) {
-    services.map(service => {
-      console.log(`seeding database for ${service}`)
+    cdServices('npm run seed')
+    cb()
+  },
+  pull: function(cb) {
+    execSync('git submodule foreach git pull origin master', {
+      stdio: 'inherit'
     })
+    cb()
+  },
+  dev: function(cb) {
+    let command = `concurrently "npm run dev"`
+    services.map(service => (comman += ` cd ${service} && npm run dev`))
+    command += 'webpack -d'
+    execSync(command, { stdio: 'inherit' })
+    cb()
   }
+}
+
+function cdServices(command) {
+  console.log('running a command in each service')
+  console.log('\n\n----------\n\n')
+  services.map(service => {
+    console.log(`running ${command} in ${service}\n\n`)
+    execSync(`cd ${service} && ${command}`, { stdio: 'inherit' })
+    console.log('\n\n----------\n\n')
+  })
 }
 
 module.exports = { ...tasks }
