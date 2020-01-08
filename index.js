157025a5399c2b66d789137bd8f43cf4a21df46c
Ubuntu <ubuntu@ip-172-31-21-241.us-east-2.compute.internal>
2020-01-08 17:35:40 +0000

    public setup

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const proxy = require('http-proxy-middleware')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const app = express()
+PORT = process.env.PORT
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '../dist')))
+
+const services = [{ route: '/details', target: process.env.SERVICE }]
+
+for ({ route, target } of services) {
+  app.use(
+    route,
+    proxy({
+      target: target,
+      changeOrigin: true
+    })
+  )
+}
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
